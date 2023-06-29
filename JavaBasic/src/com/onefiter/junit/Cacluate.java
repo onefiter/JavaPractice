ae8bf7887bcb2b950d05e2c3d099c440e3341eee
onefiter <onefiter@gmail.com>
2023-06-29 17:53:27 +0800

    junit

---
+++ JavaBasic/src/com/onefiter/junit/Cacluate.java
@@ -0,0 +1,23 @@
+package com.onefiter.junit;
+
+/**
+ * author: onefiter
+ * date: 2023/6/29
+ */
+/**
+ 业务类：实现加减乘除运算
+ */
+public class Cacluate {
+    /*
+     业务方法1：求a和b之和
+     */
+    public int sum(int a,int b){
+        return  a + b;
+    }
+    /*
+     业务方法2:求a和b之差
+     */
+    public int sub(int a,int b){
+        return  a - b;
+    }
+}
