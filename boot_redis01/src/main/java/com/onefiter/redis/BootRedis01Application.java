d8f61a2e81ecf25b24e5ae51e4aa13c553f96ceb
onefiter <onefiter@gmail.com>
2023-11-26 20:43:36 +0800

    boot redis

---
+++ boot_redis01/src/main/java/com/onefiter/redis/BootRedis01Application.java
@@ -0,0 +1,12 @@
+package com.onefiter.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRedis01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRedis01Application.class, args);
+    }
+}
---
+++ boot_redis02/src/main/java/com/onefiter/redis/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.onefiter.redis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Serializable> redisTemplate(LettuceConnectionFactory connectionFactory)
+    {
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        return redisTemplate;
+    }
+
+
+}
